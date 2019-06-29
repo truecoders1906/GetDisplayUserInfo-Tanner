324948ce16c7a8551b9c0ad2db4f7d7564d7b30e
Tanner <tanner.drayton01@gmail.com>
2019-06-29 11:46:12 -0500

    Full version 1, has all necessary questions and fixed formatting

--- GetUserInfo/GetUserInfo/Program.cs
+++ GetUserInfo/GetUserInfo/Program.cs
@@ -6,17 +6,35 @@ namespace GetUserInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Whats your name?");
-            Console.ReadLine();
+            Console.WriteLine("Hello! Whats your first name?");
+            string UserFirstName = Console.ReadLine();
+            Console.WriteLine("Nice to meet you, " + UserFirstName + "!\n" +
+                "What's Your last name?");
 
-            Console.WriteLine("Is that your full name?/n(y/n)");
-            string response = Console.ReadLine();
+            string UserLastName = Console.ReadLine();
 
-            if (response == "n")
-            {
-                Console.WriteLine("What's your last name?");
-                Console.ReadLine();
-            }
+            Console.WriteLine("So your full name is " + UserFirstName + " " + UserLastName + "?");
+
+
+            Console.WriteLine("What's your favorite number " + UserFirstName + "?");
+            string FavNumber = Console.ReadLine();
+
+            Console.WriteLine(FavNumber + " is a great number!");
+
+
+            Console.WriteLine("Do you have any pets?");
+            string HasPets = Console.ReadLine();
+
+            Console.WriteLine("Sweet! Pets are always nice to have around.");
+
+
+            Console.WriteLine("How many pets do you have?");
+            string NumOfPets = Console.ReadLine();
+
+            Console.WriteLine("So your name is " + UserFirstName + " " + UserLastName + ",\nYour favorite number is " + FavNumber + ",\n" +
+                "and lastly you have " + NumOfPets + " pets, right?");
+            Console.WriteLine("Well met friend! It was nice getting to know you! Have a good day:)");
+            
 
 
 
