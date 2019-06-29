116ac23c08e32f3ef03e4dada9c9c5738bbe62a3
Tanner <tanner.drayton01@gmail.com>
2019-06-29 11:36:45 -0500

    Version 1

--- GetUserInfo/GetUserInfo/Program.cs
+++ GetUserInfo/GetUserInfo/Program.cs
@@ -6,7 +6,24 @@ namespace GetUserInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello! Whats your name?");
+            Console.ReadLine();
+
+            Console.WriteLine("Is that your full name?/n(y/n)");
+            string response = Console.ReadLine();
+
+            if (response == "n")
+            {
+                Console.WriteLine("What's your last name?");
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
         }
     }
 }
