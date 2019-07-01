97f2343c99c35a964a41285313a770a731f1360b
Tanner <tanner.drayton01@gmail.com>
2019-07-01 13:17:24 -0500

    Full version2, added AskQuestion method, made program exception proof, added several if statements, and improved user experience with proper sentence structure based on HasPets and NumberOfPets.

--- GetUserInfo/GetUserInfo/Program.cs
+++ GetUserInfo/GetUserInfo/Program.cs
@@ -4,42 +4,79 @@ namespace GetUserInfo
 {
     class Program
     {
+
+        static string askQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Whats your first name?");
-            string UserFirstName = Console.ReadLine();
-            Console.WriteLine("Nice to meet you, " + UserFirstName + "!\n" +
-                "What's Your last name?");
+            string UserFirstName;
+            string UserLastName;
+            string UserFullName;
 
-            string UserLastName = Console.ReadLine();
+            do
+            {
+                UserFirstName = askQuestion("Hello! Whats your first name?");
+                Console.WriteLine("Nice to meet you, " + UserFirstName + "!");
 
-            Console.WriteLine("So your full name is " + UserFirstName + " " + UserLastName + "?");
+                UserLastName = askQuestion("What's your last name?");
 
+                UserFullName = askQuestion("So your full name is " + UserFirstName + " " + UserLastName + "?\n(y/n)");
+            } while (UserFullName == "n");
 
-            Console.WriteLine("What's your favorite number " + UserFirstName + "?");
-            string FavNumber = Console.ReadLine();
+            string FavNumber;
+            bool FavNumberCheck;
+
+            do
+            {
+                FavNumber = askQuestion("What is your favorite number?");
+                FavNumberCheck = int.TryParse(FavNumber, out _);
+            } while (FavNumberCheck == false);
 
             Console.WriteLine(FavNumber + " is a great number!");
 
+            string HasPets = askQuestion("Do you have any pets? (y/n)");
 
-            Console.WriteLine("Do you have any pets?");
-            string HasPets = Console.ReadLine();
+            string NumOfPets;
+            bool NumOfPetsCheck;
+            int NumberOfPets;
 
-            Console.WriteLine("Sweet! Pets are always nice to have around.");
+            if (HasPets == "y")
+            {
+                do
+                {
+                    NumOfPets = askQuestion("How many pets do you have?");
+                    NumOfPetsCheck = int.TryParse(NumOfPets, out NumberOfPets);
+                } while (NumOfPetsCheck == false);
 
 
-            Console.WriteLine("How many pets do you have?");
-            string NumOfPets = Console.ReadLine();
+                if (NumberOfPets == 1)
+                {
+                    Console.WriteLine("Awesome! A pet is always nice to have around.");
+                    Console.WriteLine("So your name is " + UserFirstName + " " + UserLastName + ",\nYour favorite number is " + FavNumber + ",\n" +
+                    "and lastly you have " + NumOfPets + " pet, right?");
+                    Console.WriteLine("Well met friend! It was nice getting to know you! Have a good day:)");
+                }
+                else if (NumberOfPets >= 2)
+                {
 
-            Console.WriteLine("So your name is " + UserFirstName + " " + UserLastName + ",\nYour favorite number is " + FavNumber + ",\n" +
+                    Console.WriteLine("Sweet! Pets are always nice to have around.");
+                    Console.WriteLine("So your name is " + UserFirstName + " " + UserLastName + ",\nYour favorite number is " + FavNumber + ",\n" +
                 "and lastly you have " + NumOfPets + " pets, right?");
-            Console.WriteLine("Well met friend! It was nice getting to know you! Have a good day:)");
-            
+                    Console.WriteLine("Well met friend! It was nice getting to know you! Have a good day:)");
+                }
+            }
 
-
-
-
-
+            else if (HasPets == "n")
+            {
+                    Console.WriteLine("That's a bummer. Pets are amazing companions.\nYou should think about getting one!\n");
+                    Console.WriteLine("So your name is " + UserFirstName + " " + UserLastName + ",\nYour favorite number is " + FavNumber + ",\n" +
+                "and lastly you have no pets, right?");
+                    Console.WriteLine("Well met friend! It was nice getting to know you! Have a good day:)");
+            }
 
 
         }
